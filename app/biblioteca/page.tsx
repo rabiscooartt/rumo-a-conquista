4866fe9ab2ed286eac80f99f374b0abef62191c9
rabiscooartt <rabiscooartt@gmail.com>
2026-09-12 20:23:30 -0300

    ajuste final no tamanho e posicionamento dos emblemas

--- app/biblioteca/page.tsx
+++ app/biblioteca/page.tsx
@@ -726,7 +726,7 @@ function GameEmblem({ game }: { game: BibliotecaGame }) {
       <div
         className={`relative h-full w-full transition duration-300 ${
           isUnlocked
-            ? "scale-[1.30]"
+            ? "translate-x-[3px] scale-[1.33]"
             : "scale-[0.98] opacity-25 blur-[5px] grayscale saturate-0 brightness-[0.45]"
         }`}
       >
