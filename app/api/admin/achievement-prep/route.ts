99daf399904790e73ca108ef4273d0f894af7855
rabiscooartt <160978762+rabiscooartt@users.noreply.github.com>
2026-09-25 21:23:59 -0300

    feat: inicializar estado nao vou fazer nas conquistas

--- app/api/admin/achievement-prep/route.ts
+++ app/api/admin/achievement-prep/route.ts
@@ -344,6 +344,7 @@ export async function GET(req: NextRequest) {
         momentary,
         journeySuggestion: journey,
         journey,
+        notDoing: false,
         id:
           "exophase-" +
           slug(registeredGame.slug) +
